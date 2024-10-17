3b5e30e3b01b48a89995f1ba0f8f68cb713c7fb8
GaryCho <honor1998321@gmail.com>
2024-10-17 14:41:41 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "static_pages/about"
   get "breeds/index"
   get "breeds/show"
+  get "about", to: "static_pages#about"
   resources :breeds, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
